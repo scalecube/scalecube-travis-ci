dd330d203803bbb0dcef5612a534cd5292337435
aharonha <aharon@marketspulse.com>
2018-04-16 17:29:08 +0300

    encrypt file manually (phase 1)

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,3 +22,16 @@ setupgit
 
 yes | travis login --github-token $GITHUBTOKEN
 
+encrypted_SOME_iv=$(date | md5sum | head -c10)
+encrypted_SOME_key=$(date | sha256sum | head -c120)
+
+travis encrypt encrypted_SOME_iv=$encrypted_SOME_iv --add
+travis encrypt encrypted_SOME_key=$encrypted_SOME_key --add
+
+## TODO create tar file
+
+
+openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in ~/tmp/secrets.tar -out secrets.tar.enc
+
+
+
