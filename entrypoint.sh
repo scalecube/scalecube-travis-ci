2e99e1ea69194a59118a333f90e272480b9dc9b7
aharonha <aharon@marketspulse.com>
2018-04-17 12:33:18 +0300

    init travis
    +encrypt secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,16 +27,22 @@ cd ~/repo
 setupgit
 
 yes | travis login --github-token $GITHUBTOKEN
-travis enable -r $GITREPONAME 
+travis enable --store-repo $GITREPONAME
+
+if [ ~ -f .travis.yml  ]; then
+	travis init java
+fi
 encrypted_SOME_iv=$(date | md5sum | head -c10)
 encrypted_SOME_key=$(date | sha256sum | head -c120)
 
-travis encrypt encrypted_SOME_iv=$encrypted_SOME_iv --add
+travis encrypt encrypted_SOME_iv=$encrypted_SOME_iv   --add
 travis encrypt encrypted_SOME_key=$encrypted_SOME_key --add
-
-## TODO create tar file
+travis encrypt SONATYPE_USERNAME=$SONATYPE_USERNAME   --add
+travis encrypt SONATYPE_PASSWORD=$SONATYPE_PASSWORD   --add
+travis encrypt GPG_PASSPHRASE=$GPG_PASSPHRASE         --add
+travis encrypt GPG_KEY=$GPG_KEY                       --add
 
 openssl aes-256-cbc -K $encrypted_key -iv $encrypted_iv -in ~/bkp/secrets.tar.enc | openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -out ~/repo/src/main/scripts/cd/secrets.tar.enc
 
-
+git status
 
