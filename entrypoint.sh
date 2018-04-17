59741cbe01713a86f5d68603b9ed90bfe33e1948
aharonha <aharon@marketspulse.com>
2018-04-17 10:50:24 +0300

    backup & re-encrypt the encrypted tar

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #! /bin/bash 
-#set -xe
+set -xe
 
 encrypted_SOME_iv=$encrypted_iv
 encrypted_SOME_key=$encrypted_key
@@ -9,19 +9,25 @@ TRAVIS_REPO_SLUG=$GITREPONAME
 cd ~
 mkdir ~/.ssh
 chmod 700 ~/.ssh
+
+#backup encrypted tar
+mkdir -p ~/bkp
+chmod 700 ~/bkp
+cp ~/src/main/scripts/cd/secrets.tar.enc ~/bkp/secrets.tar.enc
+
+
 . ~/src/main/scripts/cd/before-deploy.sh
 
 decryptsecrets
 #importpgp
 setupssh
 
-git clone git@github.com:$GITREPONAME.git repo
-cd repo
-
+git clone git@github.com:$GITREPONAME.git ~/repo
+cd ~/repo
 setupgit
 
-yes | travis login --github-token $GITHUBTOKEN
-
+travis login --github-token $GITHUBTOKEN
+travis enable -r $GITREPONAME 
 encrypted_SOME_iv=$(date | md5sum | head -c10)
 encrypted_SOME_key=$(date | sha256sum | head -c120)
 
@@ -30,8 +36,7 @@ travis encrypt encrypted_SOME_key=$encrypted_SOME_key --add
 
 ## TODO create tar file
 
-
-openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in ~/tmp/secrets.tar -out secrets.tar.enc
+openssl aes-256-cbc -K $encrypted_key -iv $encrypted_iv -in ~/bkp/secrets.tar.enc | openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -out ~/repo/src/main/scripts/cd/secrets.tar.enc
 
 
 
