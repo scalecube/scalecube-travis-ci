0999fe86504c942f3e95a1bd3b6f2efa37970aed
aharonha <aharon@marketspulse.com>
2018-04-30 10:37:37 +0300

    clean script

--- src/main/scripts/cd/deploy.sh
+++ src/main/scripts/cd/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-
 mvn -P release release:prepare release:perform -DautoVersionSubmodules=true -DscmCommentPrefix="$TRAVIS_COMMIT_MESSAGE [skip ci] " -DskipTests=true -B -V -s travis-settings.xml
-
 
 pr_master_to_dev=$(curl -u "$GITHUBUSER:$GITHUBTOKEN" -d '{"title": "Prepare for next development iteration","base": "develop" ,"head":"master"}' https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls)
 
