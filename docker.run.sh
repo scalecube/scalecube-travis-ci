f896d84441977e7247eb129adf40288ac9e3a84b
aharonha <aharon@marketspulse.com>
2018-06-03 13:02:11 +0300

    missing environment variables

--- docker.run.sh
+++ docker.run.sh
@@ -1,12 +1,12 @@
 #! /bin/bash 
 
 docker run -it --rm -e GITREPONAME=$SLUG \
-    -e encrypted_key=$encrypted_key \
-    -e encrypted_iv=$encrypted_iv \
+    -e encrypted_key=$encrypted_SOME_iv \
+    -e encrypted_iv=$encrypted_SOME_key \
     -e GITHUBTOKEN=$GITHUBTOKEN \
     -e SONATYPE_USERNAME=$SONATYPE_USERNAME \
     -e SONATYPE_PASSWORD=$SONATYPE_PASSWORD \
-    -e GPG_PASSPHRASE=$SONATYPE_PASSWORD \
+    -e GPG_PASSPHRASE=$GPG_PASSPHRASE \
     -e GPG_KEYID=$GPG_KEYID \
     -e GPG_KEY=$GPG_KEY \
     -e GITHUBUSER=$GITHUBUSER \
