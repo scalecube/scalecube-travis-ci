4140a8c4a36a7e0ed3e2af386d0e62250ab83519
aharonha <aharon@marketspulse.com>
2018-06-03 12:47:03 +0300

    docker execution for environment variables (CD enabler)

---
+++ docker.run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash 
+
+docker run -it --rm -e GITREPONAME=$SLUG \ 
+    -e encrypted_key=$encrypted_key \ 
+    -e encrypted_iv=$encrypted_iv \ 
+    -e GITHUBTOKEN=$GITHUBTOKEN \ 
+    -e SONATYPE_USERNAME=$SONATYPE_USERNAME\ 
+    -e SONATYPE_PASSWORD=$SONATYPE_PASSWORD\ 
+    -e GPG_PASSPHRASE=$SONATYPE_PASSWORD\ 
+    -e GPG_KEYID=$GPG_KEYID \ 
+    -e GPG_KEY=$GPG_KEY \ 
+    -e GITHUBUSER=$GITHUBUSER \ 
+    -e DOCKER_USERNAME=$DOCKER_USERNAME \ 
+    -e DOCKER_PASSWORD=$DOCKER_PASSWORD
