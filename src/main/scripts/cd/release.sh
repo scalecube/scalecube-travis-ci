543c83ec9fb84bdbf99ed50ef69587a47bfb0aa2
aharonha <aharon@om2.com>
2019-07-22 17:39:16 +0300

    fix script

--- src/main/scripts/cd/release.sh
+++ src/main/scripts/cd/release.sh
@@ -42,7 +42,7 @@ check_tag_for_rc() {
 check_next_version
 check_tag_for_rc
 
-echo mvn -P release -Darguments=-DskipTests release:prepare release:perform $MVN_RELEASE_VERSION $MVN_NEXT_VERSION -DautoVersionSubmodules=true -DscmCommentPrefix="$TRAVIS_COMMIT_MESSAGE [skip ci] " -B -V -s travis-settings.xml
+mvn -P release -Darguments=-DskipTests release:prepare release:perform $MVN_RELEASE_VERSION $MVN_NEXT_VERSION -DautoVersionSubmodules=true -DscmCommentPrefix="$TRAVIS_COMMIT_MESSAGE [skip ci] " -B -V -s travis-settings.xml
 
 mvn clean
 
